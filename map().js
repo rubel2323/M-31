4e02f55737bc7a1b168b1e2d2c0ce2b1589560ab
Rubel <mr.adcadre41@gmail.com>
2026-03-18 17:33:33 +0600

    map function by arrow added

---
+++ map().js
@@ -0,0 +1,10 @@
+const mapDigit = (n) => n * 2;
+const digit = [4, 5, 6, 7];
+const output = digit.map(mapDigit);
+console.log(output);
+// single line of code
+const names = ["rubel", "sumaiya", "shopon"];
+const doubled = names.map((n) => {
+  return n[0].toUpperCase();
+});
+console.log(doubled);
