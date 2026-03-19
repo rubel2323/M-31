7e2d78041e57f1807cdd92cc5b6cd607402f4d79
Rubel <mr.adcadre41@gmail.com>
2026-03-19 16:25:31 +0600

    filter find map added

--- problem-3-map-filter-find.js
+++ problem-3-map-filter-find.js
@@ -9,3 +9,12 @@ console.log(evenArr);
 // using array map
 const arrMap = oddArray.map((arrElement) => arrElement * 2);
 console.log("the arrMap is", arrMap);
+
+// filter and find
+const sampleArr = [33, 50, 79, 78, 90, 101, 30];
+
+const sampleFilter = sampleArr.filter((element) => element % 10 === 0);
+console.log("The divisibles are from filter", sampleFilter);
+// find() method
+const sampleFind = sampleArr.find((element) => element % 10 == 0);
+console.log("The divisibles are from find", sampleFind);
